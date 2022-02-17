e695e4f44c6990902ad3299617f06e1e776ce9b5
rtviii <rtkushner@gmail.com>
2022-02-17 15:34:10 -0800

    unique varieties and subgroups

--- spg.py
+++ spg.py
@@ -13,6 +13,7 @@ from farm import *
     # - farm_site_boundary_exists
     # - bound_area_ha
     # - has_3_locations
+
     # - n_locations
     # - n_crop_plans
     # - n_unique_crop_subgroups
@@ -48,45 +49,42 @@ def rectify_via_email(email:str, original_farm=False, original_userid=False):
     if len( resp ) > 1: print("Multiple users with email {}. Exiting ".format(email)); exit(1)
     [theirfams, userid, email, role] = resp[0]
     reference_farm                   = original_farm if original_farm else theirfams[0]
-    user_ids_for_farm                = get_users_for_farm(reference_farm)
-    []                = farm_fields(reference_farm)
-
+    _                                               = farm_fields(reference_farm)
+    n_crop_plans                                    = get_mps_for_usr(userid)
+    [ n_unique_varieties, n_unique_crop_subgroups ] = unique_varieties_and_subgroups(reference_farm)
     
 
 
 
-def varieties(farmid:str):
-    CUR.execute("""select array_agg(cv.crop_variety_name) as variety,
-    array_agg(c.crop_subgroup) as subgroup,
-    count(c.crop_subgroup) as subgroup_count,
-    cv.farm_id
+def farm_fields(farmid:str):
+    F            = Farm(farmid)
+    has_boundary = 'farm_site_boundary' in F.locations
+    has_3_locs   = len(F.all_poly()) >2
+    all_users    = get_users_for_farm(farmid)
+    n_locations  = len(F.all_poly())
+    return [all_users, has_boundary, has_3_locs]
+
+def unique_varieties_and_subgroups(farmid:str):
+    CUR.execute("""
+    select array_agg(cv.crop_variety_name) as variety,array_agg(c.crop_subgroup) as subgroup,cv.farm_id
     from 
     "crop_variety" cv  
-    join "crop" c on c.crop_id          = cv.crop_id
-    left join "farm" f on c.farm_id     = f.farm_id
+    left join "crop" c on c.crop_id    = cv.crop_id
+    left join "farm" f on c.farm_id    = f.farm_id
     where cv.farm_id = '%s'
-    GROUP BY cv.farm_id, f.country_id, f.grid_points"""%farmid)
-    i = CUR.fetchall()[0]
+    GROUP BY cv.farm_id"""%farmid)
+    [varieties, subgroups] = CUR.fetchall()[0]
+    return [len(list(set(varieties))), len(list(set(subgroups)))]
 
 def get_mps_for_usr(usrid:str) -> int:
     CUR.execute(
-        """
-        select count(mp.management_plan_id) as mp_number from "crop_management_plan" cmp
+    """select count(cmp.management_plan_id) as cmp_number from "crop_management_plan" cmp
     join "management_plan" mp on mp.management_plan_id = cmp.management_plan_id
     join  "users" u on mp.created_by_user_id = u.user_id
-    where u.user_id = '%s'
-        """ % usrid
-    )
+    where u.user_id = '%s'""" % usrid)
     return CUR.fetchone()[0]
 
-def usr_fields():
-    ...
 
-def farm_fields(farmid:str)->bool:
-    has_boundary         = 'farm_site_boundary' in Farm(farmid).locations
-    has_3_locs           = len(Farm(farmid).all_poly()) >2
-    print(Farm(farmid).locations.keys())
-    return True
 
 
 
@@ -102,13 +100,7 @@ def driver():
         if type("") not in [type(active),type(farmid),type(usrid), type(email) ]:
             continue# all nan/Empty row
 
-
-
         rectify_via_email(email)
-
-        
-        
-
         if type("_") == type(farmid):
             # case: about to overwrite farmid
             val = input("""Original:\t\033[95m{}\033[0m\nIncoming:\t\033[93m{}\033[0m\n\033[95m[Y] to overwrite\033[0m|\033[93m[N] to overwrite\033[0m"""
